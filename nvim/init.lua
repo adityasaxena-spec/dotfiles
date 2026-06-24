e01c532a5f19a88ac7ec3967188a42cd43cd6ca8
Aditya Saxena <adityasaxena601@gmail.com>
2026-06-24 16:09:08 +0530

    Initial commit: Adding my config files

---
+++ nvim/init.lua
@@ -0,0 +1,30 @@
+require("core.keymaps")
+require("core.options")
+require("core.colorscheme")
+
+vim.cmd([[highlight Normal guibg=none]]) -- Make the background transparent
+vim.cmd([[highlight NormalNC guibg=none]]) -- Make inactive windows transparent
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	require("plugins.neotree"),
+	require("plugins.lualine"),
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.lsp"),
+	require("plugins.autocompletion"),
+	require("plugins.autoformatting"),
+	require("plugins.gitsigns"),
+	require("plugins.indent-blankline"),
+	require("plugins.misc"),
+	require("plugins.alpha"),
+})
